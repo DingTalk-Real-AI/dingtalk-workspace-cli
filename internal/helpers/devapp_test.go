ef60d99b26167376890893a5e60df2b83feff5d0
wxianfeng <wang.fl1429@gmail.com>
2026-06-16 12:12:19 +0800

    get_robot_config

--- internal/helpers/devapp_test.go
+++ internal/helpers/devapp_test.go
@@ -170,7 +170,7 @@ func TestDevAppRobotCommandsBuildToolParams(t *testing.T) {
 		{
 			name:       "get config",
 			args:       []string{"robot", "get", "--unified-app-id", "u-1"},
-			wantTool:   "get_dev_app_robot_config",
+			wantTool:   "get_robot_config",
 			wantParams: map[string]any{"unifiedAppId": "u-1"},
 		},
 		{
